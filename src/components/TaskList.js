b292af02febdbabb09999dfe48e62345fd607752
Claisse Martinez <claisse.martinez@gmail.com>
2022-01-21 15:22:03 +0800

    Add new

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,20 +3,33 @@ import Task from "./Tasks";
 
 function TaskList(){
 
-	const tasksItemsList = [
+	// const tasksItemsList = [
+	// 	"Follow Edukasyon.ph on Facebook.",
+	// 	"Follow AWS Siklab PH on Facebook.",
+	// 	"Follow Zuitt Coding Bootcamp on Facebook.",
+	// 	"Follow Claisse Martinez on Facebook."
+
+	// ]; //index
+
+	const [taskItemsList, setTaskItemsList] = useState([
 		"Follow Edukasyon.ph on Facebook.",
 		"Follow AWS Siklab PH on Facebook.",
 		"Follow Zuitt Coding Bootcamp on Facebook.",
 		"Follow Claisse Martinez on Facebook."
 
-	]; //index
+	]);
 
 
-	const [taskValue, setTaskValue] = useState("Just another task"); //usestate resets value
+	const [taskValue, setTaskValue] = useState(""); //usestate resets value
 	console.log("taskValue: ", taskValue);
 
 	const inputChangeHandler = (e) => {
 		setTaskValue(e.target.value)
+	};
+
+	const addTaskHandler = () => {
+		setTaskItemsList([taskValue, ...taskItemsList]);
+		setTaskValue("");
 	};
 
 	return(
@@ -24,9 +37,9 @@ function TaskList(){
 		{/* <input onChange={(e) =>console.log(e.target.value)} className="task-input" placeholder="Create a new task" /> */}
 		{/* <input onChange={(e) =>setTaskValue(e.target.value)} className="task-input" placeholder="Create a new task" /> */}
 
-		<input onChange={inputChangeHandler} className="task-input" placeholder="Create a new task" />
+		<input onChange={inputChangeHandler} className="task-input" placeholder="Create a new task" onBlur={addTaskHandler} value={taskValue} />
 			<ul>
-					{tasksItemsList.map((task, index)=> {
+					{taskItemsList.map((task, index)=> {
 							console.log(task, index);
 							return <Task key={index} taskName={task} />
 						})}
